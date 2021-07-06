4f47a4f59694f33be57057a375d7bcbafabb6b4e
wemersonsantos <wemersonsantoseng@gmail.com>
2021-07-06 17:07:10 -0300

    primeiros passos em typescript

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+var meuNumero1 = 10;
+var meuNumero2 = 5;
+var resultado = somar(meuNumero1, meuNumero2);
+console.log(resultado);
---
+++ index.ts
@@ -0,0 +1,10 @@
+function somar(num1: number, num2: number){
+    return num1 + num2;
+}
+
+const meuNumero1 = 10;
+const meuNumero2 = 5;
+
+const resultado = somar(meuNumero1, meuNumero2);
+
+console.log(resultado);
