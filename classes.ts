38a13177b012377d124e95e7d22241b41ae054d5
wemersonsantos <wemersonsantoseng@gmail.com>
2021-07-11 20:39:51 -0300

    finalização do curso

--- classes.ts
+++ classes.ts
@@ -24,3 +24,49 @@ class Data2 {
         this.ano = ano;*/
     }
 }
+
+class Carro {
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMaxima: number = 220
+    ){}
+
+    private alterarVelocidade(delta: number){
+        const novaVelocidade = this.velocidadeAtual + delta;
+
+        if(novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima){
+            this.velocidadeAtual = novaVelocidade;
+        }else{
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+    }
+
+    acelerar(){
+        this.alterarVelocidade(5);
+    }
+
+    frear(){ 
+        this.alterarVelocidade(-5);
+    }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
+
+//* Herança
+class Camaro extends Carro {
+    private turbo = false;
+
+    constructor() {
+      super('Chevrolet', 'Camaro', 500);  
+    }
+
+    ligarturbo(){
+        this.turbo = true
+    }
+}
+
+const camaro = new Camaro();
